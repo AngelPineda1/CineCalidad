53559270c89270cc9ae6d2427438083f357b86e8
VANESSA <VANESSA@VANESSA-HP>
2022-06-18 13:39:49 -0500

    Add project files.

---
+++ CineCalidad/CineCalidad/Models/Pelicula.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CineCalidad.Models
+{
+    
+    public class Pelicula
+    {
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public string Sala { get; set; }
+        public string Duracion { get; set; }
+        public string Imagen { get; set; }
+
+    }
+}
---
+++ CineCalidad/CineCalidad/Views/DetallesPeliculaView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CineCalidad.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CineCalidad.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetallesPeliculaView : ContentPage
+    {
+        public DetallesPeliculaView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            gridEliminar.IsVisible = true;
+        }
+
+        private void btnNoEliminar_Clicked(object sender, EventArgs e)
+        {
+            gridEliminar.IsVisible = false;
+        }
+    }
+}
